de067e41e844cd8d4eb4520b286ea74a2b33c7c8
Joni Louhela <louhela.joni@gmail.com>
2026-01-15 22:27:20 +0200

    Fix repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cdguesser/',
+  base: '/lol-cd-guesser/',
   plugins: [react()],
   build: {
     outDir: 'dist'
